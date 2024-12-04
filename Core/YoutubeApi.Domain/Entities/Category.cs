03a04f1bd50d547f11472bb49ab4a19066069f0f
nidaozogl <ozogluniidaa@gmail.com>
2024-12-04 15:31:08 +0300

    Temel entity'ler oluşturuldu.

---
+++ Core/YoutubeApi.Domain/Entities/Category.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YoutubeApi.Domain.Common;
+
+namespace YoutubeApi.Domain.Entities
+{
+    // Category sınıfı, bir kategori nesnesini temsil ediyor.
+    // EntityBase sınıfından miras alıyor ve IEntityBase arayüzünü implemente ediyor.
+   
+    public class Category : EntityBase , IEntityBase
+    {
+        // Parametresiz bir kurucu metot. Gerekirse boş bir kategori oluşturmak için kullanılabilir.
+        
+        public Category() { }
+
+        // Parametreli bir kurucu metot. Yeni bir kategori oluştururken ParentId, Name ve Priorty değerlerini set eder.
+        
+        public Category(int parentId, string name, int priorty) 
+        {
+            ParentId = parentId;
+            Name = name;
+            Priorty = priorty;
+        }
+        
+        //Get okuma yapar(Özellikten değer okur), set yazma yapar(Özelliğe değer yazar).
+        
+        public required int ParentId { get; set; }  //(required: bu özellik mutlaka doldurulmalı.)
+        public required string Name { get; set; }
+        public required int Priorty { get; set; }
+
+        //ICollection : birden fazla öğeyi (nesneyi) bir arada tutmak için kullanılır, çünkü bir kategorinin birden çok detayı olacak.
+        public ICollection<Detail> Details { get; set; } //Burda bire çok(one-to-many) ilişki yapıldı. 
+
+        //Products ve Category çoka çok ilişkilidir.
+        public ICollection<Product> Products { get; set; } 
+
+
+    }
+}
---
+++ Core/YoutubeApi.Domain/Entities/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YoutubeApi.Domain.Common;
+
+namespace YoutubeApi.Domain.Entities
+{
+    public class Product : EntityBase
+    {
+        public required string Title { get; set; }
+        public required string Description { get; set; }
+        public required int BrandId  { get; set; }
+        public required decimal Price { get; set; }
+        public required decimal Discount { get; set; }
+        
+        public Brand Brand { get; set; }
+       // public required string ImagePath { get; set; }
+       //Bir ürünün birden fazla kategorisi olabilir. Bir kategorininde birden fazla ürünü olabilir. Bu yüzden Product ve Category sınıfında çoka çok bir ilişki kurmalıyız ki migretion oluşturduğumuzda db'de direkt çoka çok ilişki kurulsun.
+       public ICollection<Category> Categories { get; set; }
+
+    }
+}
